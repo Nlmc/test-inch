2a67bcbaf176e7b8b3b43c84452905c7adda53cd
unknown <louis.martin-compiegne@hetic.net>
2018-11-07 17:03:30 +0100

    get recurrent opening events and store them

--- event.js
+++ event.js
@@ -13,66 +13,48 @@ var Event = function(opening, recurring, startDate, endDate){
   this.endDate = endDate;
 
   if (this.opening === false) {
-    unavailableEvents.push(this);
+    unavailableEvents.push(this); // push the unavailableEvents
   }
-  else {
-    eventList.push(this);
+  else if ( this.recurring === false) {
+    eventList.push(this); // push the available events
+  } else {
+    recurrentEventList.push(this); // push the available and recurrent events
   }
 };
 
 Event.prototype.availabilities = function(fromDate, toDate){
   var dateArray = availabilities(fromDate, toDate);
+  var recurringEvents = getRecurrentEvents(fromDate, toDate);
+  var filter();
+
   return dateArray;
 };
 
 function availabilities(fromDate, toDate){
-  console.log('eventList : ', eventList, '\n unavailableEvents : ', unavailableEvents);
+  return 0;
 }
 
+// Store the day of the week and the hour
+// of start and end of recurring opening events
+function getRecurrentEvents(){
+  var recurrentDays = {};
+  for (var i = 0; i < recurrentEventList.length; i++) {
 
-function compare(availableEvents, unavailableEvents, dateOfRecurring){
-  var results = ['month', 'day', []];
-  var shortcut = results[2];
+    var event = recurrentEventList[i];
+    var begin = moment(event.startDate);
+    var end = moment(event.endDate);
 
-  for (var i = 0; i < availableEvents.length; i++) {
-    var event = availableEvents[i];
-
-    for (var k = 0; k < unavailableEvents.length; k++) {
-      var unavailable = unavailableEvents[k];
-
-      if(moment(event.startDate).day() == moment(unavailable.startDate).day()){
-        var currHour = moment(event.startDate);
-        var endHour = moment(event.endDate);
-
-        while (currHour.isBefore(endHour)){
-          if (currHour.format("HH:mm") == moment(unavailable.startDate).format("HH:mm")) {
-            var swap = [moment(unavailable.endDate).hour(), moment(unavailable.endDate).minutes()];
-            currHour.hour(swap[0]);
-            currHour.minutes(swap[1]);
-          } else {
-            shortcut.push(currHour.format("HH:mm"));
-            currHour = currHour.add(0.5, 'h');
-            }
-          }
-          results[0] = moment(dateOfRecurring).format("MMMM");
-          results[1] = moment(dateOfRecurring).date();
-        }
-      }
-    }
-    return results;
+    var day = begin.isoWeekday();
+    recurrentDays[day] = [begin.format("HH:mm"), end.format("HH:mm")];
+  }
+  console.log(recurrentDays);
+  return recurrentDays;
 }
 
-  // compare available and unavailable events to get final availabilities
+// check each days of the intervals and compare with recurrents opening events and
+// unavailable events
+function checkIfAvailable(recurrentDays, fromDate, toDate){
 
-
-
-/**
- * Check that an event is recurring and has been created before the end of given Date interval,
- * If it has been set up before Date interval ("recurring" criteria starts from creation Date),
- * then check that given event is happening in given Date interval.
- * @param {Event} event - event to process
- * @param {Date} fromDate - Start of Date interval to check for
- * @param {Date} toDate - End of Date interval to check for
- */
+}
 
 module.exports = Event;
